11a576db57abdda50c99d7ad9d3635b10de9d192
rudrodip <official.rudrodipsarker@gmail.com>
2022-09-18 23:03:39 +0600

    added dabble lib for ble connectivity

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,50 @@
 #include <Arduino.h>
-#include "sonar/create_map.h"
+#include <DabbleESP32.h>
+#include <SparkFun_TB6612.h>
+
+using namespace std;
+
+#define AIN1 25
+#define BIN1 27
+#define AIN2 33
+#define BIN2 14
+#define PWMA 32
+#define PWMB 13
+#define STBY 26
+
+const int offsetA = 1;
+const int offsetB = 1;
+
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Dabble.begin("Billu-Bot");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  Dabble.processInput();
+  Serial.print("KeyPressed: ");
+  if (GamePad.isUpPressed())
+  {
+    Serial.print("Up");
+    forward(motor1, motor2, 255);
+  } else if (GamePad.isDownPressed())
+  {
+    Serial.print("Down");
+    back(motor1, motor2, 255);
+  } else if (GamePad.isLeftPressed())
+  {
+    Serial.print("Left");
+    left(motor1, motor2, 255);
+  } else if (GamePad.isRightPressed())
+  {
+    Serial.print("Right");
+    right(motor1, motor2, 255);
+  } else
+  {
+    brake(motor1, motor2);
+  }
+  Serial.print('\t');
 }
--- src/sonar/create_map.h
+++
@@ -1,4 +0,0 @@
-#include <Arduino.h>
-using namespace std;
-
-int createMap();
